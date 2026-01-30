30aa69561005b43471a18e9d343450f0ff231856
kennek4 <kentabanay@gmail.com>
2026-01-29 17:56:13 -0700

    Color string now in Shape

--- app/src/main/java/com/example/avatar/Shape.java
+++ app/src/main/java/com/example/avatar/Shape.java
@@ -5,7 +5,7 @@ public abstract class Shape {
     private int x;
     private int y;
 
-    private String color = "blue";
+    private String color = "$YOUR_COLOR_HERE";
     public Shape(int x, int y) {
         this.x = x;
         this.y = y;
